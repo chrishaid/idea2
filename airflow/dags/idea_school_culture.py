9a05ec443451448c28215a7d492ab888b04628c7
Chris Haid <chaid@kippchicago.org>
2017-06-21 13:42:51 -0500

    Transition to actual IDEA scripts, part I

---
+++ airflow/dags/idea_school_culture.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+### Tutorial Documentation
+Documentation that goes along with the Airflow tutorial located
+[here](http://pythonhosted.org/airflow/tutorial.html)
+"""
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+
+
+# these args will get passed on to each operator
+# you can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'adhoc':False,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'trigger_rule': u'all_success'
+}
+
+dag = DAG(
+    'idea_school_culture',
+    default_args=default_args,
+    description='Pulls and prepares data for the school culture page on IEA',
+    schedule_interval=timedelta(days=1))
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='get_attendance',
+    bash_command='cd /jobs/idea/attendance && Rscript data/Attend_Enroll.R',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='get_transfers',
+    bash_command='Rscript /jobs/idea/transfers/transfers.R',
+    dag=dag)
+
+t3 = BashOperator(
+    task_id='get_tb_observations',
+    bash_command='Rscript /jobs/idea/observations/tb_observations.R',
+    dag=dag)
+
+t4 = BashOperator(
+    task_id='get_dl_suspensions',
+    bash_command='Rscript /jobs/idea/suspensions/dl_supsensions.R',
+    dag=dag)
+
+t2.set_upstream(t1)
+t3.set_upstream(t1)
+t4.set_upstream(t3)
